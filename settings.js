d4dc16b5dea02d0a6b1cd538854d2a66b553b28d
enbrau <admin@enbrau.com>
2022-05-24 15:58:49 +0800

    feat import vue3

---
+++ settings.js
@@ -0,0 +1,3 @@
+export default {
+  title: 'Sancho Seed Vue3 (Vite)'
+}
---
+++ vite.config.js
@@ -0,0 +1,39 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import settings from './settings'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, 'index.html')
+      },
+      output: {
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  },
+  plugins: [
+    vue(),
+    {
+      name: 'html-transform',
+      transformIndexHtml(html) {
+        return html.replace(
+          /<title>(.*?)<\/title>/,
+          `<title>${settings.title}</title>`
+        )
+      }
+    }
+  ],
+  server: {
+    port: 9527,
+    strictPort: false
+  }
+})
