17d6cfd6d49af5cac70a69d273b2e16f587aad00
enbrau <admin@enbrau.com>
2022-05-26 10:26:47 +0800

    feat router support

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -18,3 +18,5 @@ export const preloadHook  = createHook(new AsyncParallelHook(['app']), 'preload'
 export const requestHook  = createHook(new SyncHook(['request']),      'request',  import.meta.globEager('./request/*.js'),  'tap')
 export const responseHook = createHook(new SyncHook(['response']),     'response', import.meta.globEager('./response/*.js'), 'tap')
 export const errorHook    = createHook(new SyncHook(['error']),        'error',    import.meta.globEager('./error/*.js'),    'tap')
+export const beforeRouteHook = createHook(new AsyncParallelHook(['to', 'from', 'next']), 'beforeRoute', import.meta.globEager('./beforeRoute/*.js'),  'tapPromise')
+export const afterRouteHook  = createHook(new AsyncParallelHook(['route']),              'afterRoute',  import.meta.globEager('./afterRoute/*.js'),   'tapPromise')
